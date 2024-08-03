2ecaf8e4e176bed414cb6a8586ce8c94bac3f92f
hengyix <hengyix@uchicago.edu>
2024-08-03 02:26:04 -0500

    ui design of the shiny app

--- app.R
+++ app.R
@@ -13,4 +13,43 @@ library(plotly)
 library(rlang)
 library(RColorBrewer)
 
+# Read in database
 GTD <- read_csv('data/globalterrorismdb.csv', col_types = readr::cols())
+GTD_focus <-
+  GTD %>%
+  select(iyear, country, country_txt, gname, attacktype1, 
+         attacktype1_txt, nkill, nwound, motive) %>%
+  mutate(nhurt = nkill + nwound)
+
+ui <- 
+  dashboardPage(
+    dashboardHeader(title = 'Global Terrorism Plots'),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem('Geographic Distribution', tabName = 'tab1'),
+        menuItem('Top Perpetrators', tabName = 'tab2'),
+        menuItem('Attack Types', tabName = 'tab3')
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(
+          tabName = 'tab1', 
+          fluidRow(
+            column(9, )
+          )
+        ),
+      )
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+
