d991d2f8a06de16348f33ebfe72a2dcb846bf7af
hengyix <hengyix@uchicago.edu>
2024-08-03 06:03:43 -0500

    ui and server defined

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(shinydashboard)
 library(plotly)
 library(rlang)
 library(RColorBrewer)
+library(stringr)
 
 # Read in and clean the database 
 GTD <- read_csv('data/globalterrorismdb.csv', col_types = readr::cols())
@@ -20,8 +21,17 @@ GTD_clean <-
   select(iyear, country, country_txt, gname, attacktype1, 
          attacktype1_txt, nkill, nwound, motive) %>%
   mutate(nhurt = nkill + nwound)
+# Identify countries in South Asia
+SA <- c('Afghanistan','Bangladesh', 'Bhutan', 'India', 'Pakistan', 
+        'Maldives', 'Nepal', 'Sri Lanka') 
 
-# Define South Asia nations
+# Divide countries into three regions
+GTD_clean <-
+  GTD_clean %>%
+  mutate(region = case_when(
+    country_txt == 'Iraq' ~ 'Iraq',
+    country_txt %in% SA ~ 'South Asia',
+    .default = 'Other Regions'))
 
 # UI design
 ui <- dashboardPage(
@@ -170,8 +180,206 @@ ui <- dashboardPage(
     )
   )
 
+# Server design
+server <- function(input, output){
+  ## Tab1
+  # Heatmap
+  output$geo_attack <- renderPlotly({
+    # Prepare data for the map: matching different country names
+    GTD_clean$country_txt[GTD_clean$country_txt == 'United States'] <- 'USA'
+    GTD_map <-
+      GTD_clean %>%
+      group_by(country_txt) %>%
+      summarise(attack_count = n(), 
+                casualties = sum(nhurt, na.rm = TRUE),
+                .groups = 'drop')
+    
+    # Matching data with world dataset
+    world <- map_data('world')
+    worldSubset <- left_join(world, GTD_map, by = c('region' = 'country_txt'))
+    # Remove country out of the research 
+    worldSubset <- 
+      worldSubset %>% 
+      filter(region != 'Antarctica')
+    
+    # Draw the ggplot
+    geo_plot <- ggplot(mapping = aes(x = long, y = lat, group = group, text = region)) +
+      coord_fixed(1.3) +
+      geom_polygon(data = worldSubset, aes_string(fill = input$choice)) + 
+      scale_fill_distiller(palette ="Reds", direction = 1, na.value = 'grey') +
+      labs(title = "Geographic Distribution of Terrorist Events") + 
+      theme(
+        axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_rect(fill = "lightblue")
+        )
+    
+    # Adding interactivity
+    ggplotly(geo_plot, tooltip = c(input$choice, 'region')) 
+    })
+  
+  # Table
+  output$geo_table <- renderTable({
+    GTD_map_year <-
+      GTD_clean %>%
+      group_by(country_txt, region, iyear) %>%
+      summarise(attack_count = n(), 
+                casualties = sum(nhurt, na.rm = TRUE), 
+                .groups = 'drop') %>%
+      mutate(iyear = as.integer(iyear), 
+             attack_count = as.integer(attack_count), 
+             casualties = as.integer(casualties))
+    
+    GTD_map_year %>%
+      filter(iyear == input$yearSelect) %>%
+      arrange(desc(!!sym(input$rank_by)))
+  })
+  
+  ## Tab2
+  # Active groups
+  output$gnames_first <- renderPlotly({
+    GTD_gnames <-
+      GTD_clean %>% 
+      select(region, gname) %>%
+      group_by(region, gname) %>%
+      summarise(count = n()) %>%
+      mutate(Percentage = count/sum(count)) %>%
+      arrange(-Percentage) %>%
+      slice_head(n = 10) # Showing top 10 groups
+    
+    # Define a palette for 13 variables
+    fill_colors <- c(brewer.pal(8, "Dark2"), brewer.pal(12, "Paired"))
+    
+    gnames_first_base <- 
+      ggplot(data = subset(GTD_gnames, region == input$region_tab2)) + 
+      geom_bar(aes(y = Percentage, x = reorder(gname, -Percentage), fill = gname), 
+               stat = 'identity') + 
+      scale_fill_manual(values = fill_colors) + 
+      scale_x_discrete(label = function(x) str_trunc(x, 8)) + # Truncate xlabels
+      theme(axis.text.x = element_text(angle =45, vjust = 1, hjust=1)) + # Rotating xlabels
+      guides(fill = 'none') + 
+      theme(panel.background = element_rect(fill = '#faf6f0')) + 
+      xlab('Group Names') + 
+      ylab('Proportion') + 
+      labs(title = "Groups Responsible for Most Attacks") 
+    
+    ggplotly(gnames_first_base, tooltip = 'gname')
+  })
+  
+  # Attack count by ISIL and Taliban over time
+  # ISIL
+  output$ISIL_count <- renderPlot({
+    GTD_ISIL_time <- 
+      GTD_clean %>%
+      filter(gname == 'Islamic State of Iraq and the Levant (ISIL)') %>%
+      group_by(iyear) %>%
+      summarise(count = n())
+    
+    ggplot(data = GTD_ISIL_time) + 
+      geom_line(aes(x = iyear, y = count)) + 
+      labs(title = 'Attack Count Over Time by ISIL: from 2013') + 
+      scale_y_continuous(limits = c(0, 2000), breaks = c(0, 500, 1000, 1500, 2000)) + 
+      scale_x_continuous(breaks = c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)) +
+      xlab('Year') + 
+      ylab('Count') +
+      theme(
+        panel.background = element_rect(fill = '#faf6f0'),
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 14)
+      ) 
+  })
+  # Taliban
+  output$Taliban_count <- renderPlot({
+    GTD_Taliban_time <- 
+      GTD_clean %>%
+      filter(gname == 'Taliban') %>%
+      group_by(iyear) %>%
+      summarise(count = n())
+    
+    ggplot(data = GTD_Taliban_time) + 
+      geom_line(aes(x = iyear, y = count)) + 
+      labs(title = 'Attack Count Over Time by Taliban: from 1995') + 
+      xlab('Year') + 
+      ylab('Count') +
+      theme(
+        panel.background = element_rect(fill = '#faf6f0'),
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 14)
+      ) 
+  })
+  
+  # Tab3
+  # Attack type proportion
+  output$type_plot <- renderPlotly({
+    GTD_type_total <-
+      GTD_clean %>%
+      select(region, attacktype1_txt, gname) %>%
+      group_by(region) %>%
+      summarise(count_total = n()) # Subtotal of events by region
+    
+    GTD_type <-
+      GTD_clean %>%
+      select(region, attacktype1_txt, gname) %>%
+      group_by(region, attacktype1_txt, gname) %>%
+      summarise(count = n()) # Count of events by attack type in each region
+    
+    GTD_type <-
+      left_join(GTD_type, GTD_type_total, by = c('region')) 
+    
+    GTD_type <-
+      GTD_type %>%
+      mutate(Percentage = count/count_total) %>%
+      group_by(region, attacktype1_txt) %>%
+      summarise(Proportion = sum(Percentage))
+    
+    type_base <- ggplot(data = subset(GTD_type, region == input$region_choice)) +
+      geom_col(aes(x = reorder(attacktype1_txt, Proportion), 
+                   y = Proportion, 
+                   fill = attacktype1_txt)) + 
+      scale_x_discrete(label = function(x) str_trunc(x, 13)) + 
+      coord_flip() + 
+      scale_fill_brewer(palette = "Paired") + 
+      theme(
+        axis.text.x = element_text(angle = 0, vjust = 1, hjust=0.5),
+        panel.background = element_rect(fill = '#faf6f0'),
+        legend.position = "none"
+      ) + 
+      labs(title = "Attack Types Proportion") + 
+      xlab('Attack Types') + 
+      ylab('Proportion')
+    
+    type_plot <- ggplotly(type_base, tooltip = 'attacktype1_txt')
+  })
+  
+  # Each attack type over time
+  output$type_overtime <- renderPlot({
+    GTD_type <- 
+      GTD_clean %>%
+      group_by(attacktype1_txt, iyear,region) %>%
+      summarise(count = n())
+    #change the sort
+    GTD_type$region <- factor(GTD_type$region, levels = c("Iraq", "South Asia", "Other Regions"))
+    
+    ggplot(data = subset(GTD_type, attacktype1_txt == input$type_choice)) + 
+      geom_line(aes(x = iyear, y = count)) + 
+      facet_grid(~region) + 
+      labs(title = 'Attack Type Frequency Over Time by Region') + 
+      theme(
+        panel.background = element_rect(fill = '#faf6f0'),
+        plot.title = element_text(size = 16),
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16)
+      ) + 
+      xlab('Year') + 
+      ylab('Count')
+  })
+}
 
-
+# Run the application
+shinyApp(ui = ui, server = server)
 
 
 
